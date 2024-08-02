9c3f88fb9280a7c2549c1494b81596760bbd78ed
Mahlogonolo <ramathetje@gmail.com>
2024-08-02 11:38:11 +0200

    shop online and other modules changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const io = socketIo(server);
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Lookyard75',
+    password: 'Irene@441',
     database: 'church_app_database'
 });
 
